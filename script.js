4e91b90d91f89781ce126300abaa911f6bf2b0d4
Austin R <samichpower@yahoo.com>
2023-01-30 18:53:50 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+//WHAT DOES THIS PROJECT ENTAIL EXACTLY?
+//Simple game with five horses racing. The player begins with $1,000 and is betting with 50 other people on which horse they think will win. They all have to enter the same amount and the pool is split evenly.
+//During each course, there are 10 rounds, and in each course a single random horse has a slight advantage for all 10 rounds. 
+
