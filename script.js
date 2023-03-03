62bedcd29545532ebdb4badd7295ff419f2a6f52
Austin R <samichpower@yahoo.com>
2023-03-03 16:49:55 -0600

    Add system for deciding which horse wins

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 let doGame = confirm("Would you like to gamble on horses?")
 
-function startGame() {
-  let playersMoney = 250;
-  let roundCounter = 0;
-  while (doGame == true) {
-    let playersHorse = prompt("Which horse would you like to bet on? Red, Blue, Green, Purple, or Yellow?");
-    playersHorse = playersHorse.toUpperCase();
-
-    let playersBets = prompt(`How many bets would you like to place on the ${playersHorse} horse? CURRENT BALANCE: $${playersMoney}`);
-    playersMoney = playersMoney - (playersBets * 10);
-    console.log("You have placed $" + playersBets * 10 + " on the " + playersHorse + " horse.");
-  }
-}
-
 if (doGame == true) {
   startGame()
 } 
 
+function getWinningHorse() {
+  randNum = Math.floor(Math.random() * 6);
+  if (randNum == 0) {
+    horse = "Red";
+  } else if (randNum == 1) {
+    horse = "Blue";
+  } else if (randNum == 2) {
+    horse = "Green"
+  } else if (randNum == 3) {
+    horse = "Purple"
+  } else if (randNum == 4) {
+    horse = "Yellow"
+  }
+  return horse;
+}
 
+function startGame() {
+  let playersMoney = 250;
+  for (let roundCount = 1; roundCount < 6; roundCount++) {
+    let playersHorse = prompt("Which horse would you like to bet on? Red, Blue, Green, Purple, or Yellow?");
+    playersHorse = playersHorse.toLowerCase();
+
+    let playersBets = prompt(`How many bets would you like to place on the ${playersHorse} horse? CURRENT BALANCE: $${playersMoney}`);
+    playersMoney = playersMoney - (playersBets * 10);
+    console.log("You have placed $" + playersBets * 10 + " on the " + playersHorse + " horse.");
+    console.log(getWinningHorse())
+
+    // if (playersHorse == getWinningHorse())
+  }
+}
